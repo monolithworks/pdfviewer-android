5911b4f90ba46e348e3bcdbb4773ceab732c432d
Kenya Yoshimura <kenya_y@monolithworks.co.jp>
2018-07-07 11:58:53 +0900

    Committed initial project

---
+++ app/src/main/java/jp/co/monolithworks/imagegenerator/MainActivity.kt
@@ -0,0 +1,18 @@
+package jp.co.monolithworks.imagegenerator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+                .add(R.id.container, ViewerFragment())
+                .commit()
+    }
+
+}
